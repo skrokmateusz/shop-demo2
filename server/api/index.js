7c2ffd50b16828f069735314a1b6059843921452
skrokmateusz <skrok.mat@gmail.com>
2023-08-22 20:16:45 +0200

    change frontend domain

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 app.use(express.json())
 app.use(
 	cors({
-		origin: 'https://shop-demo2-client.vercel.app',
+		origin: 'https://shop-demo2-p1cc.vercel.app/',
 	})
 )
 
@@ -46,8 +46,8 @@ app.post('/api/create-checkout-session', async (req, res) => {
 					quantity: item.amount,
 				}
 			}),
-			success_url: `https://shop-demo2-client.vercel.app/success`,
-			cancel_url: `https://shop-demo2-client.vercel.app/cancel`,
+			success_url: `https://shop-demo2-p1cc.vercel.app//success`,
+			cancel_url: `https://shop-demo2-p1cc.vercel.app/cancel`,
 		})
 		res.json({ url: session.url })
 	} catch (e) {
